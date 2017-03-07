f9b1fe16b53979085f1e048e13c6dd725de4269d
kimcio505 <kimcio505@naver.com>
2017-03-07 10:31:44 +0900

    Update Hello_Test.java
    Git Hub Update

--- src/hello_study/Hello_Test.java
+++ src/hello_study/Hello_Test.java
@@ -5,5 +5,6 @@ public class Hello_Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Test");
 		System.out.println("Hello Two Test");
+		System.out.println("Git Hub Edit");
 	}
 }
